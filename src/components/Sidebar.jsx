c2cf416676d68023169178bc5f72ace014a3d375
Sagarika Reddi <81488244+sagarikareddi@users.noreply.github.com>
2024-01-31 21:56:49 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -13,7 +13,7 @@ function Sidebar() {
                 <img
                   src={logo}
                   alt=""
-                  className="w-16 -mt-12 rounded-full ml-6"
+                  className="w-24 rounded-full ml-6 my-5 -mt-5"
                 />
                 <li className="flex w-full justify-between text-gray-900 hover:text-gray-300 hover:bg-indigo-800 cursor-pointer items-center py-3 px-8">
                   <div className="flex items-center">
@@ -50,7 +50,9 @@ function Sidebar() {
                         d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm64 192c17.7 0 32 14.3 32 32v96c0 17.7-14.3 32-32 32s-32-14.3-32-32V256c0-17.7 14.3-32 32-32zm64-64c0-17.7 14.3-32 32-32s32 14.3 32 32V352c0 17.7-14.3 32-32 32s-32-14.3-32-32V160zM320 288c17.7 0 32 14.3 32 32v32c0 17.7-14.3 32-32 32s-32-14.3-32-32V320c0-17.7 14.3-32 32-32z"
                       />
                     </svg>
-                    <span className="text-xl  ml-2">Upload</span>
+                    <span className="text-xl text-blue-600 font-semibold ml-2">
+                      Upload
+                    </span>
                   </div>
                 </li>
                 <li className="flex w-full justify-between text-gray-900 hover:text-gray-300 hover:bg-indigo-800 cursor-pointer items-center px-8 py-3">
@@ -159,7 +161,7 @@ function Sidebar() {
                 />
               </div>
             </div>
-            <div className=" flex items-center justify-center my-[27vh] mx-auto border-2 h-[30vh] w-[30vw]">
+            <div className=" flex items-center justify-center my-[27vh] mx-auto border-2 h-fit py-5 w-[30vw]">
               <Upload />
             </div>
           </div>
